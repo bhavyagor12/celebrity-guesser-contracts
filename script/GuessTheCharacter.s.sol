a3d18fa2cb34e87d9a5103d60f9ac9aef658a54d
Bhavya Gor <bhavya.gor9999@gmail.com>
2025-04-02 00:16:43 +0530

    private key added to deployment

--- script/GuessTheCharacter.s.sol
+++ script/GuessTheCharacter.s.sol
@@ -8,7 +8,8 @@ contract DeployGuessTheCharacter is Script {
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
         new GuessTheCharacter();
         vm.stopBroadcast();
     }
