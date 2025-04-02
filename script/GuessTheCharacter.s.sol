e9fdf30111ce9edf223c59b42a4f441e128a4c64
Bhavya Gor <bhavya.gor9999@gmail.com>
2025-04-02 20:18:55 +0530

    lukso testnet deployment

--- script/GuessTheCharacter.s.sol
+++ script/GuessTheCharacter.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "forge-std/Script.sol";
 import "src/GuessTheCharacter.sol";
 
-contract DeployGuessTheCharacter is Script {
+contract DeployScript is Script {
     function setUp() public {}
 
     function run() public {
